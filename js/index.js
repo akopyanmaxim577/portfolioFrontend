55ffc0ec8f2f8ec018b09b7993c2a1ec81b3bd50
akopyanmaxim577 <akopyanmaxim57@gmail.com>
2022-10-14 08:58:08 +0000

    Email.js

--- js/index.js
+++ js/index.js
@@ -17,3 +17,42 @@ var swiper = new Swiper(".mySwiper", {
 //Aos Animate
 
 AOS.init();
+
+
+//====Contact Form
+const contactForm= document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  Message = document.getElementById('message'),
+  contactMessage = document.getElementById('contact-message');
+const sendEmail = (e) =>{
+  e.preventDefault();
+  if(
+    contactName.value === ''||
+    contactEmail.value === '' ||
+    Message.value === ''
+  ){
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+
+    contactMessage.textContent = 'Write all the input fields';
+  }else{
+    emailjs.sendForm('service_3pnytdr','template_fdv84oc','#contact-form',
+    'hClxx-RSbKyBZSFxj')
+    .then(()=>{
+      contactMessage.classList.add('color-light');
+      contactMessage.textContent = "Message sent ✔";
+
+      setTimeout(()=>{
+        contactMessage.textContent=""
+      },5000);
+    })
+    contactName.value = '';
+    contactEmail.value = '';
+    Message.value = '';
+  }
+  
+}
+
+
+contactForm.addEventListener('submit',sendEmail);
